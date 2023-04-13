95fcd58994776c854eaeed46932796b81aa43ad7
Trillium Smith <spiteless@gmail.com>
2023-04-12 17:22:36 -0700

    feat: 2023-04-13 Add 5kyu/rot13.js and tests

---
+++ codewars/javascript/5kyu/rot13.js
@@ -0,0 +1,19 @@
+export function rot13(message){
+  const converter = {}
+  const front = "abcdefghijklm"
+  const back = "nopqrstuvwxyz"
+
+  for (let i = 0; i < front.length; i++) {
+    console.log(front[i], back[i])
+    converter[front[i]] = back[i]
+    converter[front[i].toUpperCase()] = back[i].toUpperCase()
+    converter[back[i]] = front[i]
+    converter[back[i].toUpperCase()] = front[i].toUpperCase()
+  }
+
+  let out = ""
+  for (let i = 0; i < message.length; i++) {
+    out += converter[message[i]] || message[i]
+  }
+  return out
+}
---
+++ codewars/javascript/5kyu/rot13.test.js
@@ -0,0 +1,11 @@
+import { test, expect } from 'vitest'
+import { rot13 } from './rot13.js'
+
+test('handles basic rotation', () => {
+  expect(rot13('abcdefghijklm nopqrstuvwxyz')).toBe('nopqrstuvwxyz abcdefghijklm');
+});
+
+test('handles simple examples', () => {
+  expect(rot13('test')).toBe('grfg');
+  expect(rot13('Test')).toBe('Grfg');
+});
