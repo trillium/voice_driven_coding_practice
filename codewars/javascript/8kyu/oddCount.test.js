b4cbe74144b8083956d161c6cb0e4122c9ae5aa5
Trillium Smith <spiteless@gmail.com>
2023-04-15 17:43:41 -0700

    feat: 2023-04-15 Add oddCount.js and tests

---
+++ codewars/javascript/8kyu/oddCount.test.js
@@ -0,0 +1,17 @@
+import { test, expect } from 'vitest'
+import { odds } from './odds.js'
+
+test("Should handle empty array", () => {
+  expect(odds([])).toEqual([]);
+});
+test("Should handle array with even numbers only", () => {
+  expect(odds([2, 4, 6])).toEqual([]);
+});
+test("Should handle array with odd numbers only", () => {
+  expect(odds([1, 3, 5])).toEqual([1, 3, 5]);
+});
+test("Should handle mixed array", () => {
+  expect(odds([1, 2, 3, 4, 5, 6])).toEqual([1, 3, 5]);
+});
+
+
