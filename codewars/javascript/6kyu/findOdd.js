6eb1c496f0c982afac2784a848dbc28b13be6056
Trillium Smith <spiteless@gmail.com>
2023-04-20 20:16:42 -0700

    feat: 2023-04-20 Add findOdd.js and tests

---
+++ codewars/javascript/6kyu/findOdd.js
@@ -0,0 +1,15 @@
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+
+export function findOdd(A) {
+  const tracker = {};
+  while (A.length) {
+    const num = A.pop();
+    tracker[num] = tracker[num] ? tracker[num] + 1 : 1;
+  }
+  for (const [key, value] of Object.entries(tracker)) {
+    if (value % 2 === 1) {
+      return parseInt(key);
+    }
+  }
+}
