4144d8d92f01b2987d90b1fa9baf4e86f385fcb8
Trillium Smith <spiteless@gmail.com>
2023-04-22 12:27:48 -0700

    feat: 2023-04-22 Add findUniq.js and tests

---
+++ codewars/javascript/6kyu/findUniq.js
@@ -0,0 +1,6 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+export function findUniq(arr) {
+  arr.sort();
+  return arr[0] === arr[1] ? arr[arr.length - 1] : arr[0];
+}
---
+++ codewars/javascript/6kyu/findUniq.test.js
@@ -0,0 +1,11 @@
+import { test, expect } from "vitest";
+import { findUniq } from "./findUniq.js";
+
+test("Tests", () => {
+  expect(findUniq([1, 0, 0])).toBe(1);
+  expect(findUniq([0, 1, 0])).toBe(1);
+  expect(findUniq([0, 0, 1])).toBe(1);
+  expect(findUniq([1, 1, 1, 2, 1, 1])).toBe(2);
+  expect(findUniq([1, 1, 2, 1, 1])).toBe(2);
+  expect(findUniq([3, 10, 3, 3, 3])).toBe(10);
+});
