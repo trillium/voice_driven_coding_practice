62d1ffe9b766494b940bf6f50301656fe9540ad5
Trillium Smith <spiteless@gmail.com>
2023-04-23 12:28:29 -0700

    feat: 2023-04-23 Add sortArray.js and tests

---
+++ codewars/javascript/6kyu/sortArray.test.js
@@ -0,0 +1,21 @@
+import { test, expect } from "vitest";
+import { sortArray } from "./sortArray.js";
+
+test("should pass baisc tests", () => {
+  expect(sortArray([3, 1])).toStrictEqual([1, 3]);
+});
+
+test("should pass some sample tests", () => {
+  expect(sortArray([5, 3, 2, 8, 1, 4])).toStrictEqual([1, 3, 2, 8, 5, 4]);
+  expect(sortArray([5, 3, 1, 8, 0])).toStrictEqual([1, 3, 5, 8, 0]);
+  expect(sortArray([])).toStrictEqual([]);
+  expect(sortArray([1, 11, 2, 8, 3, 4, 5])).toStrictEqual([
+    1, 3, 2, 8, 5, 4, 11,
+  ]);
+});
+
+test("should pass additional tests", () => {
+  expect(sortArray([1, 11, 2, 8, 3, 4, 5])).toStrictEqual([
+    1, 3, 2, 8, 5, 4, 11,
+  ]);
+});
