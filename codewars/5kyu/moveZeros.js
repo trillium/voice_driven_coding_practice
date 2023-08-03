47060301a2b55a5c19ff9be5c7a2c28cec7ebc5f
Trillium Smith <spiteless@gmail.com>
2023-08-02 17:31:26 -0700

    feat: 2023-08-02 Add moveZerosOOfN.js and tests

--- codewars/5kyu/moveZeros.js
+++ codewars/5kyu/moveZeros.js
@@ -10,3 +10,16 @@ export function moveZeros(arr) {
   })
   return arr
 }
+
+export function moveZerosOOfN(arr) {
+  let insertPos = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      arr[insertPos++] = arr[i];
+    }
+  }
+  while (insertPos < arr.length) {
+    arr[insertPos++] = 0;
+  }
+  return arr;
+}
--- codewars/5kyu/moveZeros.test.js
+++ codewars/5kyu/moveZeros.test.js
@@ -1,7 +1,9 @@
 import { test, expect } from 'vitest'
-import { moveZeros } from './moveZeros.js'
+import { performance } from 'perf_hooks';
+import { moveZeros, moveZerosOOfN } from './moveZeros.js'
 
-test('some exampples', () => {
+
+test('some examples', () => {
   expect(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])).toStrictEqual([
     1, 2, 1, 1, 3, 1, 0, 0, 0, 0,
   ])
@@ -16,5 +18,43 @@ test('some exampples', () => {
     0,
     0,
   ])
-  moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a']) // returns[false,1,1,2,1,3,"a",0,0]
+  moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a'])
+  // returns[false,1,1,2,1,3,"a",0,0]
+})
+
+test('some examples', () => {
+  expect(moveZerosOOfN([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])).toStrictEqual([
+    1, 2, 1, 1, 3, 1, 0, 0, 0, 0,
+  ])
+  expect(moveZerosOOfN([false, 1, 0, 1, 2, 0, 1, 3, 'a'])).toStrictEqual([
+    false,
+    1,
+    1,
+    2,
+    1,
+    3,
+    'a',
+    0,
+    0,
+  ])
+  moveZerosOOfN([false, 1, 0, 1, 2, 0, 1, 3, 'a'])
+  // returns[false,1,1,2,1,3,"a",0,0]
+})
+
+test('test moveZerosOOfN is faster', () => {
+  const arr = Array(200).fill().map(() => Math.floor(Math.random() * 10));
+    // Measure execution time of moveZeros
+    let t0a = performance.now();
+    moveZeros(arr);
+    let t1a = performance.now();
+    let tza = t1a - t0a
+    console.log(`Time taken by moveZeros: ${tza} milliseconds`);
+
+    // Measure execution time of moveZerosOOfN
+    let t0b = performance.now();
+    moveZerosOOfN(arr);
+    let t1b = performance.now();
+    let tzb = t1b - t0b
+    console.log(`Time taken by moveZerosOOfN: ${tzb} milliseconds`);
+    expect()
 })
